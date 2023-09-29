3a9e0e304886e3026d9992ca9557a16363349a8c
Martin Luong <m.luong@cayas.de>
2023-09-29 16:07:34 +0200

    Retained messages

--- Broker/Broker/Program.cs
+++ Broker/Broker/Program.cs
@@ -6,6 +6,7 @@ using MQTTnet.Internal;
 using Newtonsoft.Json;
 
 const string LogFilename = "YOUR_PATH";
+const string RetainedMessagesFilename = "YOUR_PATH";
 
 var option = new MqttServerOptionsBuilder().WithDefaultEndpoint().WithDefaultEndpointPort(1884);
 
@@ -22,6 +23,59 @@ mqttServer.InterceptingPublishAsync += context =>
     File.AppendAllText(LogFilename, $"Client: {context.ClientId}, sent time: {DateTime.Now}, message: {JsonConvert.SerializeObject(message)} \r\n");
 
     return CompletedTask.Instance;
+};
+// Make sure that the server will load the retained messages.
+mqttServer.LoadingRetainedMessageAsync += async eventArgs =>
+{
+    try
+    {
+        var json = await File.ReadAllTextAsync(RetainedMessagesFilename);
+        eventArgs.LoadedRetainedMessages = JsonConvert.DeserializeObject<List<MqttApplicationMessage>>(json);
+
+        Console.WriteLine("Retained messages loaded.");
+    }
+    catch (FileNotFoundException)
+    {
+        // Ignore because nothing is stored yet.
+        Console.WriteLine("No retained messages stored yet.");
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine(exception);
+    }
+};
+
+// Make sure to persist the changed retained messages.
+mqttServer.RetainedMessageChangedAsync += async eventArgs =>
+{
+    try
+    {
+        // This sample uses the property _StoredRetainedMessages_ which will contain all(!) retained messages.
+        // The event args also contain the affected retained message (property ChangedRetainedMessage). This can be
+        // used to write all retained messages to dedicated files etc. Then all files must be loaded and a full list
+        // of retained messages must be provided in the loaded event.
+
+        //var buffer = JsonSerializer.SerializeToUtf8Bytes(eventArgs.StoredRetainedMessages);
+        //await File.WriteAllBytesAsync(RetainedMessagesFilename, buffer);
+
+        if (File.Exists(RetainedMessagesFilename) == false)
+            File.CreateText(RetainedMessagesFilename);
+
+        File.WriteAllText(RetainedMessagesFilename, JsonConvert.SerializeObject(eventArgs.StoredRetainedMessages));
+
+        Console.WriteLine("Retained messages saved.");
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine(exception);
+    }
+};
+
+// Make sure to clear the retained messages when they are all deleted via API.
+mqttServer.RetainedMessagesClearedAsync += _ =>
+{
+    File.Delete(RetainedMessagesFilename);
+    return Task.CompletedTask;
 };
 
 await mqttServer.StartAsync();
