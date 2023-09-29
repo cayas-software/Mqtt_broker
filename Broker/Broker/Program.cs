75f40feb82c052b1aee10526910b67f4490a2446
Martin Luong <m.luong@cayas.de>
2023-09-29 16:02:55 +0200

    Starting a broker

--- Broker/Broker/Program.cs
+++ Broker/Broker/Program.cs
@@ -1,1 +1,21 @@
 ﻿using System.Text.Json;
+using MQTTnet;
+using MQTTnet.Server;
+using System.Text;
+using MQTTnet.Internal;
+using Newtonsoft.Json;
+
+var option = new MqttServerOptionsBuilder().WithDefaultEndpoint().WithDefaultEndpointPort(1884);
+
+Console.WriteLine("START BUILD BROKER");
+
+var mqttServer = new MqttFactory().CreateMqttServer(option.Build());
+
+await mqttServer.StartAsync();
+
+Console.WriteLine("BROKER STARTED");
+
+Console.WriteLine("PRESS ANY KEY TO STOP BROKER");
+Console.ReadLine();
+
+await mqttServer.StopAsync();
