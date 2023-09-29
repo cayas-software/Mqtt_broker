56eb69f05a071d77d3961f3a75d4887cd5acbe5a
Martin Luong <m.luong@cayas.de>
2023-09-29 16:05:44 +0200

    Cache written messages

--- Broker/Broker/Program.cs
+++ Broker/Broker/Program.cs
@@ -5,11 +5,24 @@ using System.Text;
 using MQTTnet.Internal;
 using Newtonsoft.Json;
 
+const string LogFilename = "YOUR_PATH";
+
 var option = new MqttServerOptionsBuilder().WithDefaultEndpoint().WithDefaultEndpointPort(1884);
 
 Console.WriteLine("START BUILD BROKER");
 
 var mqttServer = new MqttFactory().CreateMqttServer(option.Build());
+mqttServer.InterceptingPublishAsync += context =>
+{
+    var message = Encoding.UTF8.GetString(context.ApplicationMessage.Payload);
+
+    if (File.Exists(LogFilename) == false)
+        File.CreateText(LogFilename);
+
+    File.AppendAllText(LogFilename, $"Client: {context.ClientId}, sent time: {DateTime.Now}, message: {JsonConvert.SerializeObject(message)} \r\n");
+
+    return CompletedTask.Instance;
+};
 
 await mqttServer.StartAsync();
 
